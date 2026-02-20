47143e3d23d1a3fc6760bdc02a891a5be8f02952
Zhang Justin <justinzhang75019@gmail.com>
2026-02-20 20:29:55 +0100

    fix: set ia_blanc to None to disable white AI in main.py

--- ECHEC-main/main.py
+++ ECHEC-main/main.py
@@ -6,7 +6,7 @@ board = Board()
 root = Tk()
 root.title("Echecs")
 
-ia_blanc = TreeIA(depth=4, enable_learning=True)
+ia_blanc = None
 ia_noir = TreeIA(depth=3, enable_learning=True)
 
 c = Chess_UI(root, board, ia_blanc, ia_noir)
